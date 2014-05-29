fdefa0891104b37aa35cd4dfce476fe22fea30b2
Péter H. Gombos <peter@gombos.no>
2014-05-29 15:55:11 +0200

    Get updates from client to layers.json

--- backend/socket.js
+++ backend/socket.js
@@ -1,5 +1,6 @@
 var sock = require('sockjs')
-  , chokidar = require('chokidar');
+  , chokidar = require('chokidar')
+  , jf = require('jsonfile');
 
 var watcher = chokidar.watch('test-project/src', {ignored: /[\/\\]\./, persistent: true});
 
@@ -9,12 +10,30 @@ connections = {};
 
 echo.on('connection', function (conn) {
   connections[conn.id] = conn;
+
   conn.on('close', function () {
     delete connections[conn.id];
   });
+
   watcher.on('all', function (event, path) {
-    conn.write(event, path);
-  })
+    if (event === 'unlink') event = 'delete';
+    console.log('Change in project detected: ' + event + ', ' + path)
+    conn.write(event + ' ' + path);
+  });
+
+  conn.on('data', function (message) {
+    // expecting a message of layerid, the field, and the value
+    // if you do something else, you are a horrible person.
+    message = message.split(' ');
+    var id = message[0], field = message[1], value = message[2];
+    jf.readFile('test-project/res/layers.json', function (err, layerlist) {
+      layerlist[id][field] = value;
+      jf.writeFile('test-project/res/layers.json', layerlist, function (err) {
+        if (err) console.log(err);
+        conn.write('layers.json updated')
+      })
+    })
+  });
 })
 
 module.exports = {socket: echo};

8a5f068f44d1fede4b5e279180ddbd84b1427f33
Péter H. Gombos <peter@gombos.no>
2014-05-29 14:59:33 +0200

    Listening on changes in test-project/src and broadcasting to websocket

--- backend/socket.js
+++ backend/socket.js
@@ -1,15 +1,19 @@
-var sock = require('sockjs');
+var sock = require('sockjs')
+  , chokidar = require('chokidar');
+
+var watcher = chokidar.watch('test-project/src', {ignored: /[\/\\]\./, persistent: true});
 
 
 var echo = sock.createServer();
 connections = {};
+
 echo.on('connection', function (conn) {
   connections[conn.id] = conn;
   conn.on('close', function () {
     delete connections[conn.id];
   });
-  conn.on('data', function (message) {
-    conn.write(message);
+  watcher.on('all', function (event, path) {
+    conn.write(event, path);
   })
 })
 

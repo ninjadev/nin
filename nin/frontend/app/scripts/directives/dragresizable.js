8244ecbc160ade410c125afa69ebee8b69093192
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2014-05-31 20:37:16 +0200

    Make dragdroppable acceptable

--- nin/frontend/app/scripts/directives/dragresizable.js
+++ nin/frontend/app/scripts/directives/dragresizable.js
@@ -2,13 +2,13 @@ angular.module('nin').directive('dragresizable', function () {
 
   var resizableConfig = {
     containment: 'parent',
-    handles: 'e, w'
+    handles: 'w, e',
+    scroll: true
   };
 
   var draggableConfig = {
     containment: 'parent',
-    axis: 'x',
-    scroll: true
+    axis: 'x'
   };
 
   return {
@@ -17,8 +17,9 @@ angular.module('nin').directive('dragresizable', function () {
       callback: '&onDragResize'
     },
     link: function postLink(scope, element, attrs) {
+      /* seems to be buggy when both draggable and resizable */
+      //element.draggable(draggableConfig);
       element.resizable(resizableConfig);
-      element.draggable(draggableConfig);
       element.on('resizestop dragstop', function (evt, ui) {
         if(scope.callback) {
           scope.callback();

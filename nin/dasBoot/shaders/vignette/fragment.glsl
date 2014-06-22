57a611bbc3c84fb3cce60afedf7c7bdf7d6c40f5
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2014-06-22 14:46:31 +0200

    Add vignette shader to dasBoot

---
+++ nin/dasBoot/shaders/vignette/fragment.glsl
@@ -0,0 +1,10 @@
+uniform sampler2D tDiffuse;
+varying vec2 vUv;
+
+void main() {
+    vec4 original = texture2D(tDiffuse, vUv);
+    float dist = length(vUv - vec2(0.5, 0.5));
+    dist = clamp(0., 1., dist / 0.707);
+    dist = pow(dist, 3.);
+    gl_FragColor = vec4(original.xyz * (1. - dist), 1.);
+}

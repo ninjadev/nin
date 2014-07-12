fb6d39d8190a44e9e711d1531c544254199afabe
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2014-07-12 12:44:33 +0200

    Update vignette

--- nin/dasBoot/shaders/vignette/fragment.glsl
+++ nin/dasBoot/shaders/vignette/fragment.glsl
@@ -5,7 +5,9 @@ varying vec2 vUv;
 void main() {
     vec4 original = texture2D(tDiffuse, vUv);
     float dist = length(vUv - vec2(0.5, 0.5));
-    dist = clamp(0., 1., dist / 0.707);
-    dist = pow(dist, 3.);
+    dist = dist / 0.707;
+    if(dist < 0.) dist = 0.;
+    if(dist > 1.) dist = 1.;
+    dist = dist * dist * dist;
     gl_FragColor = vec4(original.xyz * (1. - dist * amount), 1.);
 }

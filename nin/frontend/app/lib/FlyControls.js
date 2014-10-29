e7e1ac0ed6bccab7af1bcb4b7495f113ec663b50
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2014-10-29 05:23:22 +0100

    Tweak flycontrols

--- nin/frontend/app/lib/FlyControls.js
+++ nin/frontend/app/lib/FlyControls.js
@@ -12,7 +12,8 @@ THREE.FlyControls = function ( object, domElement ) {
   // API
 
   this.movementSpeed = 1.0;
-  this.rollSpeed = 0.005;
+  this.movementSpeedMultiplier = .1;
+  this.rollSpeed = 1;
 
   this.dragToLook = false;
   this.autoForward = false;
@@ -49,7 +50,7 @@ THREE.FlyControls = function ( object, domElement ) {
 
     switch ( event.keyCode ) {
 
-      case 16: /* shift */ this.movementSpeedMultiplier = .1; break;
+      case 16: /* shift */ this.movementSpeedMultiplier = 0.0005; break;
 
       case 87: /*W*/ this.moveState.forward = 1; break;
       case 83: /*S*/ this.moveState.back = 1; break;
@@ -80,7 +81,7 @@ THREE.FlyControls = function ( object, domElement ) {
 
     switch( event.keyCode ) {
 
-      case 16: /* shift */ this.movementSpeedMultiplier = 1; break;
+      case 16: /* shift */ this.movementSpeedMultiplier = 0.01; break;
 
       case 87: /*W*/ this.moveState.forward = 0; break;
       case 83: /*S*/ this.moveState.back = 0; break;
@@ -110,8 +111,8 @@ THREE.FlyControls = function ( object, domElement ) {
 
   this.update = function( delta ) {
 
-    var moveMult = delta * this.movementSpeed;
-    var rotMult = delta * this.rollSpeed;
+    var moveMult = delta * this.movementSpeed * this.movementSpeedMultiplier;
+    var rotMult = delta * this.rollSpeed * this.movementSpeedMultiplier;
 
     this.object.translateX( this.moveVector.x * moveMult );
     this.object.translateY( this.moveVector.y * moveMult );

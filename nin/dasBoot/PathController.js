a7fd32e10412035f2bccbbc3c884d37bf91dc816
Stian Jensen <me@stianj.com>
2015-07-06 01:47:56 +0200

    Make movement types optional in cameracontroller
    3D paths with only two key points are also supported now.
    This means the following will be possible:
    
    ```
    {
      "startFrame": 100,
      "endFrame": 1000,
      "points": [
        [1,2,3],
        [4,5,6],
        [7,8,9]
      ]
    }
    ...
    {
      "startFrame": 100,
      "endFrame": 1000,
      "points": [1,2,3]
    }
    ...
    {
      "startFrame": 100,
      "endFrame": 1000,
      "points": [
        [1,2,3],
        [4,5,6]
      ]
    }
    ```
    
    In addition, 3D paths may now follow a circle instead of a spline:
    
    ```
    {
      "startFrame": 100,
      "endFrame": 1000,
      "type": "circle",
      "center": [0, 0, 0],
      "radius": 100
    }
    ```
    
    Thus concludes this commit message. The wiki will explain the format in
    more detail.

--- nin/dasBoot/PathController.js
+++ nin/dasBoot/PathController.js
@@ -17,20 +17,56 @@ PathController.prototype.parse3Dkeyframes = function(keyframes) {
       endFrame: keyframes[i].endFrame,
       easing: keyframes[i].easing
     };
-    if (this_pos.type == "spline") {
-      var points = [];
-      for (var j=0; j<keyframes[i].points.length; j++) {
-        var point = keyframes[i].points[j];
-        points[j] = new THREE.Vector3(point[0], point[1], point[2]);
+    if (this_pos.type == 'circle') {
+      this_pos.radius = keyframes[i].radius;
+      this_pos.center = new THREE.Vector3(
+        keyframes[i].center[0], keyframes[i].center[1], keyframes[i].center[2]
+      );
+      if (keyframes[i].up) {
+        var up = new THREE.Vector3(
+          keyframes[i].up[0], keyframes[i].up[1], keyframes[i].up[2]
+        );
+        this_pos.angle = new THREE.Vector3(0, 1, 0).angleTo(up);
+        this_pos.rotator = new THREE.Vector3(0, 1, 0).cross(up).normalize();
+      } else {
+        this_pos.angle = 0;
+        this_pos.rotator = new THREE.Vector3(0, 1, 0);
       }
-      this_pos.points = new THREE.SplineCurve3(points);
-    } else if (this_pos.type == "point") {
-      var point = new THREE.Vector3(
-          keyframes[i].point[0],
-          keyframes[i].point[1],
-          keyframes[i].point[2]
-          );
-      this_pos.point = point;
+    } else if (keyframes[i].point || keyframes[i].points) {
+      var raw_points = keyframes[i].point || keyframes[i].points;
+      if (raw_points.length == 1) {
+        this_pos.type = 'point';
+        var point = new THREE.Vector3(
+            raw_points[0][0],
+            raw_points[0][1],
+            raw_points[0][2]
+            );
+        this_pos.point = point;
+      } else if (typeof raw_points[0] === "number" && raw_points.length == 3) {
+        this_pos.type = 'point';
+        var point = new THREE.Vector3(
+            raw_points[0],
+            raw_points[1],
+            raw_points[2]
+            );
+        this_pos.point = point;
+      } else if (raw_points.length == 2) {
+        this_pos.type = 'linear';
+        this_pos.from = new THREE.Vector3(
+          raw_points[0][0], raw_points[0][1], raw_points[0][2]
+        );
+        this_pos.to = new THREE.Vector3(
+          raw_points[1][0], raw_points[1][1], raw_points[1][2]
+        );
+      } else if (raw_points.length > 2) {
+        this_pos.type = 'spline';
+        var points = [];
+        for (var j=0; j<raw_points.length; j++) {
+          var point = raw_points[j];
+          points[j] = new THREE.Vector3(point[0], point[1], point[2]);
+        }
+        this_pos.points = new THREE.SplineCurve3(points);
+      }
     }
     parsed[i] = this_pos;
   }
@@ -46,11 +82,21 @@ PathController.prototype.parseKeyframes = function(keyframes) {
       endFrame: keyframes[i].endFrame,
       easing: keyframes[i].easing
     };
-    if (current.type == "transition") {
-      current.from = keyframes[i].from;
-      current.to = keyframes[i].to;
-    } else if (current.type == "fixed") {
-      current.value = keyframes[i].value;
+    var raw_points = keyframes[i].value || keyframes[i].point || keyframes[i].points;
+    if (raw_points) {
+      if (typeof raw_points === "number") {
+        current.type = 'value';
+        current.value = raw_points;
+      } else if (raw_points.length == 1) {
+        current.type = 'value';
+        current.value = raw_points[0];
+      } else if (raw_points.length == 2) {
+        current.type = 'linear';
+        current.from = keyframes[i].from;
+        current.to = keyframes[i].to;
+      } else if (raw_points.length > 2) {
+        console.warn("A maximum two points is currently supported");
+      }
     }
     parsed[i] = current;
   }
@@ -59,30 +105,53 @@ PathController.prototype.parseKeyframes = function(keyframes) {
 
 PathController.prototype.get3Dpoint = function(frame) {
   var current = this.getCurrentPath(frame);
+  var duration = current.endFrame - current.startFrame;
+  var t = (frame - current.startFrame) / duration;
+  if (current.easing == "smoothstep") {
+    t = smoothstep(0, 1, t);
+  } else if (current.easing == "easeIn") {
+    t = easeIn(0, 1, t);
+  } else if (current.easing == "easeOut") {
+    t = easeOut(0, 1, t);
+  }
 
-  if (current.type == "spline") {
+  if (current.type == 'spline') {
     if (frame > current.endFrame) {
       return current.points.points[current.points.points.length-1];
     }
-    var spline_duration = current.endFrame - current.startFrame;
-    var t = (frame - current.startFrame) / spline_duration;
-    var s_t = t;
-    if (current.easing == "smoothstep") {
-      s_t = smoothstep(0, 1, t);
-    } else if (current.easing == "easeIn") {
-      s_t = easeIn(0, 1, t);
-    } else if (current.easing == "easeOut") {
-      s_t = easeOut(0, 1, t);
+    return current.points.getPointAt(t);
+
+  } else if (current.type == 'linear') {
+    if (frame > current.endFrame) {
+      return current.to;
     }
-    return current.points.getPointAt(s_t);
+    return new THREE.Vector3(
+      lerp(current.from.x, current.to.x, t),
+      lerp(current.from.y, current.to.y, t),
+      lerp(current.from.z, current.to.z, t)
+    );
+
+  } else if (current.type == 'point') {
+    return current.point;
+  } else if (current.type == 'circle') {
+    if (frame > current.endFrame) {
+      t = 0;
+    }
+    var point = new THREE.Vector3(
+      Math.sin(t * Math.PI * 2) * current.radius,
+      0,
+      Math.cos(t * Math.PI * 2) * current.radius
+    );
+    point.applyAxisAngle(current.rotator, current.angle);
+    point.add(current.center);
+    return point;
   }
-  return current.point;
 };
 
 PathController.prototype.getPoint = function(frame) {
   var current = this.getCurrentPath(frame);
 
-  if (current.type == "transition") {
+  if (current.type == 'linear') {
     if (frame > current.endFrame) {
       return current.to;
     }
@@ -96,8 +165,9 @@ PathController.prototype.getPoint = function(frame) {
       return easeIn(current.from, current.to, t);
     }
     return lerp(current.from, current.to, t);
+  } else if (current.type == 'value') {
+    return current.value;
   }
-  return current.value;
 };
 
 PathController.prototype.getCurrentPath = function(frame) {

76fc35d38cda8d68532202c060c9f89a5cf12de0
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2015-07-06 15:20:49 +0200

    Add BEATBEAN music syncing
    The bpm and subdivisions are currently hardcoded, which is ok for now.

--- nin/dasBoot/loop.js
+++ nin/dasBoot/loop.js
@@ -12,12 +12,27 @@ function createLoop(options) {
     this.deltaTime = 0;
     this.currentFrame = 0;
 
+    BEAN = 0;
+    BEAT = false;
+    var subdivision = 6;
+    var beatsPerMinute = 110 * subdivision;
+    var beatsPerSecond = beatsPerMinute / 60;
+    var framesPerSecond = 60;
+    var framesPerBeat = framesPerSecond / beatsPerSecond;
+    var frameSyncOffset = 0;
+
     var that = this;
     this.loop = function() {
       that.time = music.currentTime * 1000;
       that.deltaTime += that.time - that.oldTime;
       that.oldTime = that.time;
       while(that.deltaTime >= frameLength) {
+        BEAT = false;
+        if((((that.currentFrame + frameSyncOffset - 1) / framesPerBeat) | 0) >
+            ((that.currentFrame + frameSyncOffset - 2) / framesPerBeat) | 0) {
+          BEAT = true;
+        }
+        BEAN = that.currentFrame / framesPerBeat | 0;
         update(that.currentFrame++);
         that.deltaTime -= frameLength;
       }

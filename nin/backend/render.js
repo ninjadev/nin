17524d0a7554619a288767641e67e0d4e8f69def
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2015-07-15 23:01:13 +0200

    Add nin render command
    nin render stiches together already-generated .png frames to a single
    .mp4 video. You need avconv installed to run this.

---
+++ nin/backend/render.js
@@ -0,0 +1,22 @@
+var spawn = require('child_process').spawn;
+
+var render = function(projectPath) {
+  var avconv = spawn('avconv', [
+  '-y',
+  '-r', '60',
+  '-i', projectPath + '/bin/render/%07d.png',
+  '-itsoffset', '-00:00:00.133',
+  '-i', projectPath + '/res/music.mp3',
+  '-c:v', 'libx264',
+  '-c:a', 'copy',
+  '-crf', '16',  
+  projectPath + '/bin/render/render.mp4']);
+  avconv.stdout.on('data', function(data) {
+    console.log(data.toString());
+  });
+  avconv.stderr.on('data', function(data) {
+    console.log(data.toString());
+  });
+}
+
+module.exports = {render: render};

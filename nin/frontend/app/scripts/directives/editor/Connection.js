ac2bcde8cbfe51c48c76292abefd9ea988665e4a
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-03-16 23:09:10 +0100

    Remove broken cruft in GraphEditor
    We can now no longer manipulate the UI to add and remove connections.
    That is ok, though, since the functionality was never actually there in
    the first place, it was just a quick'n'dirty UI prototype.

--- nin/frontend/app/scripts/directives/editor/Connection.js
+++ nin/frontend/app/scripts/directives/editor/Connection.js
@@ -10,20 +10,6 @@ class Connection extends React.Component {
     };
   }
 
-  /*
-  onMouseOver(event) {
-    this.setState({
-      showDeleteButton: true,
-    });
-  }
-
-  onMouseOut(event) {
-    this.setState({
-      showDeleteButton: false,
-    });
-  }
-  */
-
   delete() {
     this.props.editor.removeConnection(
       this.props.fromPath,
@@ -37,8 +23,6 @@ class Connection extends React.Component {
     const midY = this.props.connection.from.y + dY / 2;
     const intensity = Math.abs(Math.cos(Math.atan(dX/dY)));
     return e('g', {
-      //onMouseOver: event => this.onMouseOver(event),
-      //onMouseOut: event => this.onMouseOut(event),
     }, e('path', {
       d: `M${this.props.connection.from.x} ${this.props.connection.from.y} Q${this.props.connection.from.x + dX / 2 * intensity } ${this.props.connection.from.y} ${midX} ${midY} T${this.props.connection.to.x} ${this.props.connection.to.y}`,
       stroke: 'white',
@@ -47,21 +31,7 @@ class Connection extends React.Component {
       style: {
         pointerEvents: 'none',
       }
-    }),
-    this.state.showDeleteButton ? e('circle', {
-      className: 'delete-button',
-      cx: midX,
-      cy: midY,
-      r: 10 / this.props.scale,
-      onClick: () => this.delete(),
-    }) : null,
-    this.state.showDeleteButton ? e('text', {
-      className: 'delete-button',
-      x: 0,
-      y: 0,
-      transform: `translate(${midX}, ${midY}) scale(${1 / this.props.scale})`,
-      onClick: () => this.delete(),
-    }, '⨯') : null);
+    }));
   }
 }
 

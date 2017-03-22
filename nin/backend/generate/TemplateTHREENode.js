a3304fc487e2c63ae19b2fcde1431e1e084d6288
Stian Jensen <me@stianj.com>
2017-03-22 23:50:46 +0100

    Add generate command for THREENodes

---
+++ nin/backend/generate/TemplateTHREENode.js
@@ -0,0 +1,30 @@
+(function(global) {
+  class TemplateTHREENode extends NIN.THREENode {
+    constructor(id, options) {
+      super(id, {
+        outputs: {
+          render: new NIN.TextureOutput()
+        }
+      });
+
+      this.cube = new THREE.Mesh(new THREE.BoxGeometry(50, 5, 5),
+                                 new THREE.MeshBasicMaterial({ color: 0x000fff }));
+      this.scene.add(this.cube);
+
+      var light = new THREE.PointLight(0xffffff, 1, 100);
+      light.position.set(50, 50, 50);
+      this.scene.add(light);
+
+      this.camera.position.z = 100;
+    }
+
+    update(frame) {
+      super.update(frame);
+
+      this.cube.rotation.x = Math.sin(frame / 10);
+      this.cube.rotation.y = Math.cos(frame / 10);
+    }
+  }
+
+  global.TemplateTHREENode = TemplateTHREENode;
+})(this);

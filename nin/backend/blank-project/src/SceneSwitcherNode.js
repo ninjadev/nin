d9213093effa4e844267e2026cc608470c23f827
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2017-04-23 19:53:50 +0200

    Add default SceneSwitcherNode to ease new project startup

---
+++ nin/backend/blank-project/src/SceneSwitcherNode.js
@@ -0,0 +1,32 @@
+(function(global) {
+  class SceneSwitcherNode extends NIN.Node {
+    constructor(id) {
+      super(id, {
+        inputs: {
+          A: new NIN.TextureInput(),
+          B: new NIN.TextureInput(),
+        },
+        outputs: {
+          render: new NIN.TextureOutput(),
+        }
+      });
+    }
+
+    update() {
+      this.inputs.A.enabled = false;
+      this.inputs.B.enabled = false;
+
+      let selectedScene;
+      if (BEAN < 48 * 4) {
+        selectedScene = this.inputs.A;
+      } else {
+        selectedScene = this.inputs.B;
+      }
+
+      selectedScene.enabled = true;
+      this.outputs.render.setValue(selectedScene.getValue());
+    }
+  }
+
+  global.SceneSwitcherNode = SceneSwitcherNode;
+})(this);

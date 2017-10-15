738ec450f30cb963526bd42bdb915a7bb2f312b1
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-10-15 15:29:23 +0200

    Update editor Connection to use JSX

--- nin/frontend/app/scripts/components/editor/Connection.js
+++ nin/frontend/app/scripts/components/editor/Connection.js
@@ -22,19 +22,19 @@ class Connection extends React.Component {
     const midX = this.props.connection.from.x + dX / 2;
     const midY = this.props.connection.from.y + dY / 2;
     const intensity = Math.abs(Math.cos(Math.atan(dX/dY)));
-    const attributes = {
-      d: `M${this.props.connection.from.x} ${this.props.connection.from.y} Q${this.props.connection.from.x + dX / 2 * intensity } ${this.props.connection.from.y} ${midX} ${midY} T${this.props.connection.to.x} ${this.props.connection.to.y}`,
-      stroke: this.props.connection.active ? 'white' : '#7a8185',
-      strokeWidth: this.props.connection.active ? 5 / this.props.scale : 1 / this.props.scale,
-      fill: 'transparent',
-      style: {
-        pointerEvents: 'none',
-      }
-    };
-    if(!this.props.connection.active) {
-      attributes.strokeDasharray = '20, 10';
-    }
-    return e('g', {}, e('path', attributes));
+    const d = `M${this.props.connection.from.x} ${this.props.connection.from.y} Q${this.props.connection.from.x + dX / 2 * intensity } ${this.props.connection.from.y} ${midX} ${midY} T${this.props.connection.to.x} ${this.props.connection.to.y}`;
+    return (
+      <g>
+        <path
+          d={d}
+          stroke={this.props.connection.active ? 'white' : '#7a8185'}
+          strokeWidth={this.props.connection.active ? 5 / this.props.scale : 1 / this.props.scale}
+          fill="transparent"
+          pointerEvents="none"
+          strokeDasharray={this.props.connection.active ? undefined : '20, 10'}
+        />
+      </g>
+    );
   }
 }
 
